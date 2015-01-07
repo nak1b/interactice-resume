3561645d4a82dfa0ab15c3ac48b9ba9ce4a9018a
Nakib <nbmomin14@gmail.com>
2015-01-06 23:53:33 -0500

    Added skills and Work experience

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,10 +9,50 @@ var bio = {
 		"location" : "Toronto"
 	},
 	"welcomeMessage" : "Welcome to My Online Resume",
-	"skills" : ["Web Programming", "Software Development", "Problem Solving", "Learning", "Time Management"],
+	"skills" : ["Web Programming ", "Software Development ", "Problem Solving ", "Learning ", "Time Management "],
 	"picture" : "images/fry.jpg"
 }
 
+
+var work = {
+	"position" : "Freelace Developer",
+	"employeer" : "Freelace.com",
+	"dates" : "May 2014 - Dec 2014 ",
+	"location" : "Online",
+	"description" : "Designing and developing website for clients"
+}
+
+var project = {
+	"title" : "Personal Website",
+	"dates" : "2014",
+	"description" : "Designed and Develope website to showcase my personal as well as academic achievements using latest web technologies like HTML, CSS, Javascript, jQuery, PHP and twitter bootstrap as base.",
+	"images" : ["image1", "image2"] 
+}
+
+var education = {
+	"school" : {
+		"name": "Seneca College",
+		"degree" : "Computer Programming",
+		"yearStarted" : "2013-2015",
+		"city" : "Toronto"
+	},
+	"OnlineCourses" : [
+		{
+			"title":"Javascript Basics",
+			"school" : "Udacity",
+			"dates" : "2015",
+			"url" : "http://www.udacity.com"
+		},
+
+		{
+			"title": "Intro To Computer Science",
+			"school" : "Udacity",
+			"dates" : "2013",
+			"url" : "http://www.udacity.com"
+		}
+	]
+
+} 
 
 
 //formating values for header sections
@@ -39,4 +79,38 @@ $("#topContacts").prepend(formatedMobile);
 
 $("#header").prepend(formatedRole);
 $("#header").prepend(formatedName);
+
+
+//formating value for work section
+//var formatedWorkStart = HTMLworkStart.replace("%data%", work.workStart);
+var formatedWorkTitle = HTMLworkEmployer.replace("%data%", work.employeer) + HTMLworkTitle.replace("%data%", work.position);
+var formatedWorkLocation = HTMLworkLocation.replace("%data%", work.location);
+var formatedWordDesc = HTMLworkDescription.replace("%data%", work.description);
+var formatedWordDate = HTMLworkDates.replace("%data%", work.dates);
+
+
+//appending work data
+
+$("#workExperience").append(HTMLworkStart);
+$(".work-entry").prepend(formatedWordDesc);
+$(".work-entry").prepend(formatedWordDate);
+$(".work-entry").prepend(formatedWorkTitle);
+
+var formatedSkills = HTMLskills.replace("%data%", bio.skills.join("    "));
+
+if(bio.skills.length > 0)
+{
+	$("#header").append(HTMLskillsStart);
+	for(var i = 0; i<bio.skills.length; i++)
+	{
+		var formatedSkills = HTMLskills.replace("%data%", bio.skills[i]);
+		$("#skills").append(formatedSkills);
+	}
+	
+}
+//formatting value for education
+var formatedSchoolStart = HTMLschoolStart.replace("%data%", education.school.yearStarted);
+var formatedSchoolName = HTMLschoolStart.replace("%data%", education.school.name);
+var formatedSchoolDegree = HTMLschoolStart.replace("%data%", education.school.degree);
+var formatedSchoolLocation = HTMLschoolStart.replace("%data%", education.school.location);
 
